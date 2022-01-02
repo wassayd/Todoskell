d4fa0ca0b7d3ee6d6402773d17d40f74565b5634
wassayd <saidwaskar@gmail.com>
2022-01-02 21:11:33 +0100

    add aeson and text dependecy and create a todo in json file

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
+{-# LANGUAGE OverloadedStrings, DeriveGeneric, DeriveAnyClass #-}
+
 module Main where
 
-import Lib ( someFunc )
 
-main :: IO ()
-main = someFunc
+import GHC.Generics
+import Data.Text.Lazy (Text)
+import Data.Text.Lazy.IO as I
+import Data.Aeson.Text (encodeToLazyText)
+import Data.Aeson (ToJSON)
+
+data Todo = Todo {
+    todo :: Text 
+   ,isDone :: Bool 
+} deriving (Show, Generic, ToJSON)
+
+meowmers = Todo { todo = "learn haskell", isDone = False } 
+
+main = I.writeFile "todos.json" (encodeToLazyText meowmers)
