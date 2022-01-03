3fa3a4a25925c1fd0cb2aa0cda6945c836a7648c
wassayd <saidwaskar@gmail.com>
2022-01-03 22:33:35 +0100

    WIP

--- app/Main.hs
+++ app/Main.hs
@@ -7,13 +7,30 @@ import GHC.Generics
 import Data.Text.Lazy (Text)
 import Data.Text.Lazy.IO as I
 import Data.Aeson.Text (encodeToLazyText)
-import Data.Aeson (ToJSON)
+import Data.Aeson
+import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Char8 as C
 
 data Todo = Todo {
     todo :: Text 
    ,isDone :: Bool 
-} deriving (Show, Generic, ToJSON)
+} deriving (Show, Generic, ToJSON, FromJSON)
 
-meowmers = Todo { todo = "learn haskell", isDone = False } 
 
-main = I.writeFile "todos.json" (encodeToLazyText meowmers)
+jsonFile :: FilePath
+jsonFile = "todos.json"
+
+exempleTodo :: Todo
+exempleTodo = Todo { todo = "learn haskell", isDone = False } 
+
+main :: IO ()
+main = I.writeFile jsonFile $ encodeToLazyText exempleTodo
+
+ 
+getJSON :: IO B.ByteString
+getJSON = B.readFile jsonFile
+
+ 
+decodeJSON :: IO ()
+decodeJSON = do json <- getJSON
+                Prelude.putStrLn $ C.unpack json
