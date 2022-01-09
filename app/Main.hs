5e99363c2208df048ddab5c5a424938fad005390
wassayd <saidwaskar@gmail.com>
2022-01-09 23:13:38 +0100

    wip add todo

--- app/Main.hs
+++ app/Main.hs
@@ -10,14 +10,14 @@ import Data.String (IsString)
 
 data Todo = Todo {
     pos     :: Int,
-    todo    :: String , 
-    isDone  :: Bool 
+    todo    :: String ,
+    isDone  :: Bool
 } deriving (Show, Generic, ToJSON, FromJSON)
 
 
 
 isArgValid :: (Eq a, IsString a) => a -> Bool
-isArgValid "add"      = True 
+isArgValid "add"      = True
 isArgValid "delete"   = True
 isArgValid "show"     = True
 isArgValid "edit"     = True
@@ -26,16 +26,20 @@ isArgValid "reverse"  = True
 isArgValid "clear"    = True
 isArgValid "quit"     = True
 isArgValid "help"     = True
-isArgValid _          = False 
+isArgValid _          = False
 
 jsonFile :: FilePath
 jsonFile = "todos.json"
 
-exempleTodo :: Todo
-exempleTodo = Todo { pos = 1, todo = "learn haskell", isDone = False } 
+
+
+createTodo :: String -> IO Todo
+createTodo str = do
+    nbTodo <- countTodos
+    return Todo { pos = nbTodo + 1 , todo = str, isDone = False }
 
 displayCommands :: IO ()
-displayCommands = do 
+displayCommands = do
     putStrLn "Commands:"
     putStrLn "add       <String> - Add a TODO entry"
     putStrLn "delete    <Int>    - Delete the numbered entry"
@@ -46,21 +50,23 @@ displayCommands = do
     putStrLn "clear              - Clear todo"
     putStrLn "quit               - Quit"
     putStrLn "help               - Help"
-    
+
 main :: IO ()
-main = do    
+main = do
     displayCommands
     arg <- getLine
     putStrLn $ "Vous avez saisie " ++ arg
-    if isArgValid arg 
-        then Prelude.putStrLn "Valid"
-        else Prelude.putStrLn "Invalid"
+    if isArgValid arg
+        then
+            case arg of
+                "add" -> addTodo
+                _     -> putStrLn "Une erreur c'est produite"
+        else putStrLn "Invalid"
 
- 
+
 getJSON :: IO B.ByteString
 getJSON = B.readFile jsonFile
 
- 
 decodeJSON :: IO (Maybe [Todo])
 decodeJSON = do
     jsonData <- getJSON
@@ -68,16 +74,27 @@ decodeJSON = do
     return rse
 
 
+
+
+countTodos :: IO Int
+countTodos = maybe 0 length <$> decodeJSON
+
 saveTodo :: Todo -> IO ()
 saveTodo a = do
     todos <- decodeJSON
-    case todos of 
+    case todos of
         Just todosArr  -> B.writeFile jsonFile $ encode $ todosArr ++ [a]
-        Nothing        -> B.writeFile jsonFile $ encode [a] 
-    
- 
-    
-addTodo = undefined
+        Nothing        -> B.writeFile jsonFile $encode [a]
+
+
+
+addTodo :: IO ()
+addTodo = do
+    putStrLn "Ecrire todo fast"
+    str <- getLine
+    todo <- createTodo str
+    saveTodo todo
+
 
 deleteTodo = undefined
 
